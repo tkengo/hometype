4ff38a92fe1b20afb17e44829117ecbf76051572
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-05-30 22:19:34 +0900

    add keycode constant

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
   $(document).keydown(function(event) {
     var top = document.body.scrollTop;
     console.log(event.keyCode);
-    if (event.keyCode == 74) {
+    if (event.keyCode == KeyCode.J) {
       $('html, body').animate({ scrollTop: (top + 50) + 'px' }, 100);
     }
-    else if (event.keyCode == 75) {
+    else if (event.keyCode == KeyCode.K) {
       $('html, body').animate({ scrollTop: (top - 50) + 'px' }, 100);
     }
-    else if (event.keyCode == 67) {
+    else if (event.keyCode == KeyCode.C) {
       caretBrowseMode = !caretBrowseMode;
       $('html').attr('contenteditable', caretBrowseMode);
     }
