1b510cfd0d0a87ca055b4eacc75a568d2643808f
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-07-03 06:23:51 +0900

    cancel key event when visual mode

--- js/mode/visual.js
+++ js/mode/visual.js
@@ -1,5 +1,9 @@
 var VisualMode = function() {
 };
 
-VisualMode.prototype.onKeyDown = function(key) {
+VisualMode.prototype.onKeyDown = function(key, currentKey, e) {
+  if ($.inArray(currentKey, [ 'Down', 'Up', 'Right', 'Left' ]) == -1) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
 };
