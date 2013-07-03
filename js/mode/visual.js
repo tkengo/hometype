0163455676f60d905f5920743a3dacfa8e521b1a
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-07-04 06:26:52 +0900

    key down is accepted in visual mode if meta key is down

--- js/mode/visual.js
+++ js/mode/visual.js
@@ -2,7 +2,8 @@ var VisualMode = function() {
 };
 
 VisualMode.prototype.onKeyDown = function(key, currentKey, e) {
-  if ($.inArray(currentKey, [ 'Down', 'Up', 'Right', 'Left' ]) == -1) {
+  var isMetaKey = e.ctrlKey || e.altKey || e.shiftKey || e.metaKey;
+  if (!isMetaKey && $.inArray(currentKey, [ 'Down', 'Up', 'Right', 'Left' ]) == -1) {
     e.stopPropagation();
     e.preventDefault();
   }
