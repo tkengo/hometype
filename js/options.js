64e5b9d3da070b3fd71855945e42399eb7619fb9
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-03-17 14:35:23 +0900

    Rewrite option object from scratch with singleton pattern

--- js/options.js
+++ js/options.js
@@ -4,53 +4,56 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *
  * Manage options.
- *
- * Create instance and initialize it.
- * var options = new HometypeOptions();
- * options.init();
- *
- * Option object has option value in object prototype.
- * You can access to option value below:
- * options.option_name1
- * options.option_name2
  */
-var HometypeOptions = function() {
-  this.options = HometypeDefaultOptions;
+var HometypeOptions = (function() {
+  /**
+   * Singleton instance of hometype option.
+   */
+  var instance = null;
 
-  // Set an event listener to notify for this object when option value was changed.
-  var port = chrome.runtime.connect({ name: 'notifyOptions' });
-  port.onMessage.addListener($.proxy(this.notifyOptions, this));
-};
+  /**
+   * Hometype options value.
+   */
+  var options = {};
 
-/**
- * Callback method that is called when option value was changed.
- * Changed option value will be saved this object.
- *
- * @param Object results Changed option value hash.
- */
-HometypeOptions.prototype.notifyOptions = function(results) {
-  this.options = $.extend(this.options, results);
-  for (var key in results) {
-    HometypeOptions.prototype[key] = results[key];
-  }
-};
-
-/**
- * Initialization. All option is loaded into this object.
- *
- * @param function callback Callback method that is called when option has loaded.
- */
-HometypeOptions.prototype.init = function(callback) {
-  var getOptionsCallback = function(response) {
-    for (var key in response) {
-      this.options[key] = response[key];
-      HometypeOptions.prototype[key] = this.options[key];
+  /**
+   * Set options value to this object. This method is invoked in initialization
+   * and receiving a notification from event page script when options value was
+   * changed.
+   */
+  function setOptions(loadedOptions)
+  {
+    for (var key in results) {
+      options[key] = results[key];
     }
+  }
 
-    if (typeof callback == 'function') {
-      callback();
+  /**
+   * Create object for singleton instance.
+   */
+  function createInstance()
+  {
+    return {
+      options: options
+    };
+  }
+
+  // Load options.
+  chrome.runtime.sendMessage({ command: 'getOptions' }, setOptions);
+
+  // Set event listener to notify for this object when options value was changed.
+  chrome.runtime.connect({ name: 'notifyOptions' }).onMessage.addListener(setOptions);
+
+  // Return an object that has only 'getInstance' method to get singleton instance.
+  return {
+    getInstance: function() {
+      if (instance == null) {
+        instance = createInstance();
+      }
+
+      return instance;
     }
   };
+})();
 
-  chrome.runtime.sendMessage({ command: 'getOptions' }, $.proxy(getOptionsCallback, this));
-};
+var Opt = HometypeOptions.getInstance().options;
