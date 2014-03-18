41822f4f384c871d1e1dde8447bc9b902bc0b496
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-03-18 15:27:02 +0900

    Set default key bindings

--- js/default_option.js
+++ js/default_option.js
@@ -1,4 +1,54 @@
 HometypeDefaultOptions = {
   command_interval: 300,
-  scroll_amount: 50
+  scroll_amount: 50,
+  nmap: {
+    'j': 'scrollDown',
+    'k': 'scrollUp',
+    'G': 'scrollToBottom',
+    'gg': 'scrollToTop',
+    'f': 'enterHintMode',
+    'F': 'enterNewWindowHintMode',
+    'x': 'closeTab',
+    'v': 'enterVisualMode',
+    '<C-e>': 'scrollDown',
+    '<C-y>': 'scrollUp',
+    '<C-d>': 'scrollDownHalf',
+    '<C-u>': 'scrollUpHalf',
+    '<C-f>': 'scrollDownPage',
+    '<C-b>': 'scrollUpPage',
+    '<C-p>': 'moveLeftTab',
+    '<C-n>': 'moveRightTab',
+    ':': 'enterCommandMode',
+    'b': 'searchBookmarks',
+    'B': 'searchBookmarks',
+    'i': 'enterInsertMode',
+    'a': 'enterInsertMode',
+    'I': 'focusFirstInput',
+    'A': 'focusLastInput',
+    'H': 'backHistory',
+    'L': 'forwardHistory',
+    'u': 'restoreTab',
+    'U': 'searchClosedTabs',
+    '<C-h>': 'searchHistories'
+  },
+  cmap: {
+    'Esc': 'cancelCommandMode',
+    '<C-n>': 'selectNextCandidate',
+    '<C-p>': 'selectPrevCandidate'
+  },
+  imap: {
+    'Esc': 'cancelInsertMode',
+    '<C-c>': 'cancelInsertMode',
+    '<C-p>': 'moveLeftTab',
+    '<C-n>': 'moveRightTab'
+  },
+  vmap: {
+    'Esc': 'cancelVisualMode',
+    'n': 'forwardContentEditable',
+    'p': 'backwardContentEditable'
+  },
+  fmap: {
+    'Esc': 'cancelHintMode',
+    '<C-c>': 'cancelHintMode'
+  }
 };
