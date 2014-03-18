4cfa197cb3afa7e83048bf773de98e6b36ff6eaa
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-03-19 06:03:13 +0900

    Added key_bind key in option to be more useful

--- options/main.js
+++ options/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-  chrome.runtime.sendMessage({ command: 'getOptions' }, function(response) {
-    for (var key in response) {
-      $('#' + key).val(response[key]);
-    }
+  HometypeOptions.getInstance().load(function(options) {
+    $.each(options, function(key, value) {
+      var element = $('#' + key);
+      if (element.length > 0) {
+        element.val(value);
+      }
+    });
+
+    var bindList = '';
+    $.each(options.key_bind, function(map, bind) {
+      $.each(bind, function(key, command) {
+        bindList += map + ' ' + key + ' ' + command + "\r\n";
+      });
+      bindList += "\r\n";
+    });
+    $('#key_bind_list').val(bindList);
   });
 
   $('#save').click(function() {
@@ -13,13 +25,18 @@ $(document).ready(function() {
     });
 
     var bindList = $('#key_bind_list').val();
+    params.key_bind = {};
     $.each(bindList.split(/\r\n|\r|\n/), function(index, line) {
-      value = line.split(' ');
-      if (!params[value[0]]) {
-        params[value[0]] = {};
+      if (line == '') {
+        return true;
       }
 
-      params[value[0]][value[1]] = value[2];
+      value = line.split(' ');
+      if (!params.key_bind[value[0]]) {
+        params.key_bind[value[0]] = {};
+      }
+
+      params.key_bind[value[0]][value[1]] = value[2];
     });
     chrome.runtime.sendMessage({ command: 'setOptions', params: params });
   });
