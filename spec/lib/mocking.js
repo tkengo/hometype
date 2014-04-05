750f574c1b39add336812c3ac800b700097f358d
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-04-05 15:49:06 +0900

    Removed unnecessary codes and Added necessary mocking

--- spec/lib/mocking.js
+++ spec/lib/mocking.js
@@ -2,29 +2,15 @@ var listeners = {};
 
 chrome = {
   runtime: {
+    sendMessage: function(params, callback) {
+    },
     connect: function(connectInfo) {
       return {
         onMessage: {
           addListener: function(listener) {
-            listeners['chrome.runtime.connect.onMessage'] = listener;
           }
         }
       };
-    }
-  }
-};
-
-mock = {
-  fireEvent: function() {
-    var event = arguments[0];
-    var params = [];
-
-    for (var i = 1; i < arguments.length; i++) {
-      params.push(arguments[i]);
-    }
-
-    if (listeners[event]) {
-      listeners[event].apply(this, params);
     }
   }
 };
