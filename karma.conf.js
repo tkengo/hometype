b432bb764d5f946568e6fc76826e7a50f79aa767
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-04-05 15:15:42 +0900

    Reduced karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,7 @@
-// Karma configuration
-// Generated on Sat Apr 05 2014 12:13:28 GMT+0900 (JST)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
       'spec/lib/jasmine-2.0.0/jasmine.js',
       'spec/lib/jasmine-2.0.0/jasmine-html.js',
@@ -25,57 +13,24 @@ module.exports = function(config) {
       'js/options.js',
       'spec/**/*.js'
     ],
-  // <script type="text/javascript" src="lib/jasmine-2.0.0/jasmine.js"></script>
-  // <script type="text/javascript" src="lib/jasmine-2.0.0/jasmine-html.js"></script>
-  // <script type="text/javascript" src="lib/jasmine-2.0.0/boot.js"></script>
-
-
-    // list of files to exclude
-    exclude: [
-      
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    exclude: [ ],
     preprocessors: {
-     'js/*.js': [ 'coverage' ]
+     'js/**/*.js': [ 'coverage' ]
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: [
+      'progress',
+      'coverage'
+    ],
     coverageReporter: {
       type: 'html',
       dir : 'coverage/'
     },
 
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
   });
 };
