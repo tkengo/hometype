1a676ffede0591550719f329776da95b6035fa43
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-04-22 05:33:52 +0900

    Added make.sh to create zip file for publish to chrome webstore

---
+++ make.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+BASE_DIR=$(cd $(dirname $0);pwd)
+ZIP_FILE=$BASE_DIR/hometype.zip
+PUBLISH_DIR=$BASE_DIR/__publish
+IGNORE_FILES="
+.git
+.gitignore
+make.sh
+README.md
+karma.conf.js
+package.json
+wercker.yml
+spec
+"
+
+if [ -e "$ZIP_FILE" ]; then
+    rm -f $ZIP_FILE
+fi
+
+git clone git@github.com:tkengo/hometype.git $PUBLISH_DIR
+
+for ignore_file in $IGNORE_FILES; do
+    rm -fr $PUBLISH_DIR/$ignore_file
+done
+
+cd $BASE_DIR && zip -r $ZIP_FILE ${PUBLISH_DIR##*/}
+rm -fr $PUBLISH_DIR
