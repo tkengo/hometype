c3f8bd0527da745415c1e89c1f2be5dbb73bfed4
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-07-02 11:47:26 +0900

    Add Jp library

--- js/homedics.js
+++ js/homedics.js
@@ -34,65 +34,6 @@ Homedics.dictLetters = [
 ];
 
 /**
- * Conversion map of roman to hiragana.
- */
-Homedics.conversionMap = {
-  'a': 'あ', 'i': 'い', 'u': 'う', 'e': 'え', 'o': 'お',
-  'k': { 'a': 'か', 'i': 'き', 'u': 'く', 'e': 'け', 'o': 'こ',
-    'y': { 'a': 'きゃ', 'i': 'きぃ', 'u': 'きゅ', 'e': 'きぇ', 'o': 'きょ' }
-  },
-  's': { 'a': 'さ', 'i': 'し', 'u': 'す', 'e': 'せ', 'o': 'そ',
-    'y': { 'a': 'しゃ', 'i': 'しぃ', 'u': 'しゅ', 'e': 'しぇ', 'o': 'しょ' },
-    'h': { 'a': 'しゃ', 'i': 'し',   'u': 'しゅ', 'e': 'しぇ', 'o': 'しょ' }
-  },
-  't': { 'a': 'た', 'i': 'ち', 'u': 'つ', 'e': 'て', 'o': 'と',
-    'y': { 'a': 'ちゃ', 'i': 'ちぃ', 'u': 'ちゅ', 'e': 'ちぇ', 'o': 'ちょ' },
-    'h': { 'a': 'てゃ', 'i': 'てぃ', 'u': 'てゅ', 'e': 'てぇ', 'o': 'てょ' }
-  },
-  'n': { 'a': 'な', 'i': 'に', 'u': 'ぬ', 'e': 'ね', 'o': 'の',
-    'y': { 'a': 'にゃ', 'i': 'にぃ', 'u': 'にゅ', 'e': 'にぇ', 'o': 'にょ' },
-    'n': 'ん',
-  },
-  'h': { 'a': 'は', 'i': 'ひ', 'u': 'ふ', 'e': 'へ', 'o': 'ほ',
-    'y': { 'a': 'ひゃ', 'i': 'ひぃ', 'u': 'ひゅ', 'e': 'ひぇ', 'o': 'ひょ' }
-  },
-  'm': { 'a': 'ま', 'i': 'み', 'u': 'む', 'e': 'め', 'o': 'も',
-    'y': { 'a': 'みゃ', 'i': 'みぃ', 'u': 'みゅ', 'e': 'みぇ', 'o': 'みょ' }
-  },
-  'y': { 'a': 'や', 'u': 'ゆ', 'o': 'よ' },
-  'r': { 'a': 'ら', 'i': 'り', 'u': 'る', 'e': 'れ', 'o': 'ろ',
-    'y': { 'a': 'りゃ', 'i': 'りぃ', 'u': 'りゅ', 'e': 'りぇ', 'o': 'りょ' }
-  },
-  'w': { 'a': 'わ', 'o': 'を' },
-  'g': { 'a': 'が', 'i': 'ぎ', 'u': 'ぐ', 'e': 'げ', 'o': 'ご',
-    'y': { 'a': 'ぎゃ', 'i': 'ぎぃ', 'u': 'ぎゅ', 'e': 'ぎぇ', 'o': 'ぎょ' }
-  },
-  'z': { 'a': 'ざ', 'i': 'じ', 'u': 'ず', 'e': 'ぜ', 'o': 'ぞ',
-    'y': { 'a': 'じゃ', 'i': 'じぃ', 'u': 'じゅ', 'e': 'じぇ', 'o': 'じょ' }
-  },
-  'j': { 'a': 'じゃ', 'i': 'じ', 'u': 'じゅ', 'e': 'じぇ', 'o': 'じょ' },
-  'd': { 'a': 'だ', 'i': 'ぢ', 'u': 'づ', 'e': 'で', 'o': 'ど',
-    'y': { 'a': 'ぢゃ', 'i': 'ぢぃ', 'u': 'ぢゅ', 'e': 'ぢぇ', 'o': 'ぢょ' },
-    'h': { 'a': 'でゃ', 'i': 'でぃ', 'u': 'でゅ', 'e': 'でぇ', 'o': 'でょ' }
-  },
-  'b': { 'a': 'ば', 'i': 'び', 'u': 'ぶ', 'e': 'べ', 'o': 'ぼ',
-    'y': { 'a': 'びゃ', 'i': 'びぃ', 'u': 'びゅ', 'e': 'びぇ', 'o': 'びょ' }
-  },
-  'p': { 'a': 'ぱ', 'i': 'ぴ', 'u': 'ぷ', 'e': 'ぺ', 'o': 'ぽ',
-    'y': { 'a': 'ぴゃ', 'i': 'ぴぃ', 'u': 'ぴゅ', 'e': 'ぴぇ', 'o': 'ぴょ' }
-  },
-  'c': { 'a': 'か', 'i': 'し', 'u': 'く', 'e': 'せ', 'o': 'こ',
-    'y': { 'a': 'ちゃ', 'i': 'ちぃ', 'u': 'ちゅ', 'e': 'ちぇ', 'o': 'ちょ' },
-    'h': { 'a': 'ちゃ', 'i': 'ち',   'u': 'ちゅ', 'e': 'ちぇ', 'o': 'ちょ' }
-  },
-  'q': { 'a': 'くぁ', 'i': 'くぃ', 'u': 'く', 'e': 'くぇ', 'o': 'くぉ' },
-  'f': { 'a': 'ふぁ', 'i': 'ふぃ', 'u': 'ふ', 'e': 'ふぇ', 'o': 'ふぉ' },
-  'l': { 'a': 'ぁ', 'i': 'ぃ', 'u': 'ぅ', 'e': 'ぇ', 'o': 'ぉ' },
-  'x': { 'a': 'ぁ', 'i': 'ぃ', 'u': 'ぅ', 'e': 'ぇ', 'o': 'ぉ' },
-  'v': { 'a': 'ヴぁ', 'i': 'ヴぃ', 'u': 'ヴ', 'e': 'ヴぇ', 'o': 'ヴぉ' }
-};
-
-/**
  * Build regular expression for dictionary matching.
  *
  * @param string roman
@@ -103,7 +44,7 @@ Homedics.prototype.buildRegexp = function(roman) {
     return null;
   }
 
-  var hiragana    = this.getHiraganaCandidates(roman);
+  var hiragana    = Jp.getHiraganaCandidates(roman);
   var dict        = this.loadDict(roman.charAt(0));
   var dictPattern = new RegExp('^(' + hiragana.join('|') + ').*:(.*)$', 'gm');
   var patterns    = [];
@@ -123,7 +64,7 @@ Homedics.prototype.buildRegexp = function(roman) {
   }
   if (hiragana) {
     regexp.push(hiragana.join('|'));
-    regexp.push(this.toKatakana(hiragana.join('|')));
+    regexp.push(Jp.toKatakana(hiragana.join('|')));
   }
 
   if (regexp.length > 0) {
@@ -156,123 +97,6 @@ Homedics.prototype.loadDict = function(letter) {
   xhr.open('GET', dictUrl, false);
   xhr.send();
   return this.dict = xhr.responseText;
-};
-
-/**
- * Get hiragana candidates.
- *
- * For example:
- * If roman is 'sar', then return 'さら', 'さり', 'さる', 'され', 'さろ'
- * If roman is 'kak', then return 'かか', 'かき', 'かく', 'かけ', 'かこ'
- *
- * @param string roman A roman string.
- * @return array Hiragana candidates.
- */
-Homedics.prototype.getHiraganaCandidates = function(roman) {
-  var hiragana = this.roman2hiragana(roman);
-  var halfRoman, candidate = [];
-
-  if (halfRoman = hiragana.match(/[a-z]+$/)) {
-    hiragana = hiragana.replace(/[a-z]/g, '');
-
-    var converted = this.convertHalfRomanToHiragana(halfRoman[0]);
-    for (var i = 0; i < converted.length; i++) {
-      candidate.push(hiragana + converted[i]);
-    }
-  } else {
-    candidate.push(hiragana);
-  }
-
-  return candidate;
-};
-
-/**
- * Convert hiragana to katakana.
- *
- * @param string target A string that converts from hiragana to katakana.
- */
-Homedics.prototype.toKatakana = function(target) {
-  return target.replace(/[ぁ-ん]/g, function(s) {
-    return String.fromCharCode(s.charCodeAt(0) + 0x60);
-  });
-};
-
-/**
- * Convert half roman to hiragana.
- *
- * For example:
- * If roman is 'a',  then return 'あ'
- * If roman is 'k',  then return 'か', 'き', 'く', 'け', 'こ', 'きゃ', 'きぃ', 'きゅ', 'きぇ', 'きょ'
- * If roman is 'ky', then return 'きゃ', 'きぃ', 'きゅ', 'きぇ', 'きょ'
- *
- * @param string roman A roman string.
- * @return array Hiragana candidates.
- */
-Homedics.prototype.convertHalfRomanToHiragana = function(roman, depth) {
-  var candidate = [];
-
-  if (roman.length == 1) {
-    var map = Homedics.conversionMap[roman];
-    if (typeof map == 'object') {
-      for (var i in map) {
-        if (typeof map[i] == 'object') {
-          for (var j in map[i]) {
-            candidate.push(map[i][j]);
-          }
-        } else {
-          candidate.push(map[i]);
-        }
-      }
-    } else {
-      candidate.push(map);
-    }
-  } else if (roman.length == 2) {
-    var map = Homedics.conversionMap[roman.charAt(0)][roman.charAt(1)];
-    for (var i in map) {
-      candidate.push(map[i]);
-    }
-  }
-
-  return candidate;
-};
-
-/**
- * Convert a roman string to a hiragana.
- *
- * @param string roman A string you want to convert.
- */
-Homedics.prototype.roman2hiragana = function(roman) {
-  var hiragana = '';
-
-  for (var i = 0; i < roman.length; i++) {
-    var str = roman.charAt(i),
-        map = Homedics.conversionMap[str];
-
-    while (true) {
-      if (typeof map == 'string') {
-        break;
-      } else {
-        var c = roman.charAt(++i);
-        if (!(map = map[c])) {
-          i--;
-          break;
-        }
-        str += c;
-      }
-    }
-
-    if (map) {
-      hiragana += map;
-    } else if (str == roman.charAt(i + 1)) {
-      hiragana += 'っ';
-    } else if (str == 'n') {
-      hiragana += 'ん';
-    } else {
-      hiragana += str;
-    }
-  }
-
-  return hiragana;
 };
 
 /**
